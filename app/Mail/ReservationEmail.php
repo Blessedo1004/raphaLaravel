cc688e87e3e24fc7c00e72682b8552b7cc14d86f
Blessedo <blessedvictorigwe@gmail.com>
2025-11-06 14:05:02 +0100

    commit

---
+++ app/Mail/ReservationEmail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\PendingReservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ReservationEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The reservation instance.
+     *
+     * @var \App\Models\PendingReservation
+     */
+    public $reservation;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(PendingReservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Reservation Details',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'rapha.emails.reservation-email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
