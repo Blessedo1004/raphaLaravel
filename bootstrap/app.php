0ba377e5bac15bda913ce4c5de425bf13d4c5b33
Blessedo <blessedvictorigwe@gmail.com>
2026-02-05 13:27:12 +0100

    docker

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app->middleware([
-    \App\Http\Middleware\TrustProxies::class,
-]);
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'preregister.notice' => \App\Http\Middleware\EnsurePregistrationNotice::class,
