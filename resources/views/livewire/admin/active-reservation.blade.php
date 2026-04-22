57401bf417e7385ceb1675a72ba8273ae4111b68
Blessedo <blessedvictorigwe@gmail.com>
2026-04-22 23:51:17 +0100

    added transitions

--- resources/views/livewire/admin/active-reservation.blade.php
+++ resources/views/livewire/admin/active-reservation.blade.php
@@ -23,31 +23,48 @@
       @if($reservations->isEmpty())
         <h4 class="text-center mt-4">No reservations found</h4>
       @else
-        @foreach ($reservations->groupBy(function($reservation) {
-            return $reservation->created_at->format('Y-m-d');
-        }) as $date => $reservationsOnDate)
-        <div class="col-12 bg-light mt-3 mx-auto d-block" wire:transition>
+      @foreach ($reservations->groupBy(function($reservation) {
+          return $reservation->created_at->format('Y-m-d');
+      }) as $date => $reservationsOnDate)
+
+      <div 
+          class="col-12 bg-light mt-3 mx-auto d-block"
+          wire:key="date-{{ $date }}"
+          wire:transition
+      >
           <h3 class="text-center mt-5 date_heading">
-            @if(Carbon\Carbon::parse($date)->isToday())
-              Today
-            @elseif(Carbon\Carbon::parse($date)->isYesterday())
-              Yesterday
-            @else
-              {{ Carbon\Carbon::parse($date)->format('F j, Y') }}
-            @endif
+              @if(Carbon\Carbon::parse($date)->isToday())
+                  Today
+              @elseif(Carbon\Carbon::parse($date)->isYesterday())
+                  Yesterday
+              @else
+                  {{ Carbon\Carbon::parse($date)->format('F j, Y') }}
+              @endif
           </h3>
+
           @foreach ($reservationsOnDate as $reservation)
-            <div class="text-black mx-auto d-block col-11 col-md-8 reservation_div mt-4 py-2 mb-3" wire:click="showDetails({{ $reservation->id }})">
-                <div class="row justify-content-center">
-                  <h5 class="col-12 col-sm-6 col-xl-4 text-center text-md-start">{{$reservation->user->last_name . " " . $reservation->user->first_name }}</h5>
-                  <h5 class="col-12 col-sm-6 col-xl-4 text-center text-md-start">{{$reservation->room->name}}</h5>
-                </div>
-                
-                <h6 class="mt-2 text-center">{{$reservation->created_at->format('g:i A')}}</h6>
-            </div>
+              <div 
+                  class="text-black mx-auto d-block col-11 col-md-8 reservation_div mt-4 py-2 mb-3"
+                  wire:key="reservation-{{ $reservation->id }}"
+                  wire:click="showDetails({{ $reservation->id }})"
+              >
+                  <div class="row justify-content-center">
+                      <h5 class="col-12 col-sm-6 col-xl-4 text-center text-md-start">
+                          {{ $reservation->user->last_name . " " . $reservation->user->first_name }}
+                      </h5>
+                      <h5 class="col-12 col-sm-6 col-xl-4 text-center text-md-start">
+                          {{ $reservation->room->name }}
+                      </h5>
+                  </div>
+
+                  <h6 class="mt-2 text-center">
+                      {{ $reservation->created_at->format('g:i A') }}
+                  </h6>
+              </div>
           @endforeach
-          </div>
-        @endforeach
+
+      </div>
+      @endforeach
         @if(method_exists($reservations, 'links'))
         <div class="d-flex justify-content-center mt-4">
             {{ $reservations->links() }}
